337529fff29377c1da642baabaea159516cbb70a
CORP\idan.berman <idan.berman@sisense.com>
2017-07-04 14:41:41 +0300

    change logo

--- myScript.js
+++ myScript.js
@@ -4,3 +4,7 @@ var width = '150';
 var iframe = "<iframe src='"+url+"' id='sisenseInsightBall' height="+height+" width="+width+" frameborder='0'></iframe>";
 
 $('body').append(iframe);
+
+$(document).ready(function(){
+  $('#sisenseInsightBall').draggable();
+});
--- sisenseInsight.js
+++ sisenseInsight.js
@@ -1,10 +1,3 @@
 $(document).ready(function(){
-  $( "#insight-ball" ).hover(
-    function() {
-      $(this).children().addClass('large');
-    },
-    function(){
-      $(this).children().removeClass('large');
-    }
-  );
+
 });
