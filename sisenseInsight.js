6a0211d423961537aac78a66415b8cfb065a3876
CORP\idan.berman <idan.berman@sisense.com>
2017-07-05 15:49:58 +0300

    add move button and show content

--- sisenseInsight.js
+++ sisenseInsight.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
+  $('#insight-ball').click(function() {
+    if (!($('#insights-content').is(":visible"))){
+      $('#insights-content').show("blind");
+      $('#insight-ball-btn').show("drop");
+      $('#insight-ball').addClass('active');
+    }
+  })
+});
 
+// hides the div when clicking outside of the component
+$(document).mouseup(function(e)
+{
+    var container = $("#insights-content");
+    // if the target of the click isn't the container nor a descendant of the container
+    if(!($(e.target).parents('#sisense-insights').length > 0))
+    {
+        container.hide("blind");
+        $('#insight-ball').removeClass('active');
+        $('#insight-ball-btn').hide("drop");
+    }
 });
