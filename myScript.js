c6b07c1d647484223c13e90e49371abb6663594b
CORP\idan.berman <idan.berman@sisense.com>
2017-07-17 13:05:31 +0300

    add listener to the input field of gmail whastapp

--- myScript.js
+++ myScript.js
@@ -2,12 +2,48 @@
 var height = '150';
 var width = '150';
 var iframe = "<iframe src='"+url+"' id='sisenseInsightBall' height="+height+" width="+width+" frameborder='0'></iframe>";
+*/
 
-$('body').append(iframe);
+//$('body').append(iframe);
+
+//
+var activeListener = false;
+if(location.origin == "https://mail.google.com"){
+  setInterval(function(){
+    if (document.querySelector('[role="textbox"]') && !activeListener){
+      var myText = $('div[role="textbox"]');
+      $('div[role="textbox"]').bind("DOMSubtreeModified",function(){
+        console.log(myText.html());
+        chrome.runtime.sendMessage(myText.html());
+      });
+      activeListener = true;
+    }else if (!document.querySelector('[role="textbox"]')){
+      $('div[role="textbox"]').unbind("DOMSubtreeModified");
+      activeListener = false;
+      console.log("no input");
+    }
+  }, 3000);
+}
+else if(location.origin == "https://web.whatsapp.com"){
+  setInterval(function(){
+    if (document.querySelector('[class="input"]') && !activeListener){
+      var myText = $('div[class="input"]');
+      $('div[class="input"]').bind("DOMSubtreeModified",function(){
+        console.log(myText.html());
+        chrome.runtime.sendMessage(myText.html());
+      });
+      activeListener = true;
+    }else if (!document.querySelector('[class="input"]')){
+      $('div[class="input"]').unbind("DOMSubtreeModified");
+      activeListener = false;
+      console.log("no input");
+    }
+  }, 3000);
+}
 
 $(document).ready(function(){
-  $('#sisenseInsightBall').draggable();
-});*/
+
+});
 
 $.get(chrome.runtime.getURL('sisenseInsight.html'), function(data) {
     //alert(data);
